765ed4efc7684bdf47ecea4c4c531325c611dcb1
hassan <hassan.tariq@gmail.com>
2023-07-27 01:33:07 +0500

    Added background image

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './components/Login'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h2>Magic motion 🚀</h2>
+      <Router>
+        <Routes>
+          <Route exact path='/' element={<Login/>}>            
+          </Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
